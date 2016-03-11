59208d7678ca3fc944ff822fa14e504fb03a84e3
EthanAdner <ethanda99@gmail.com>
2016-03-11 14:13:09 -0500

    Updated from Brython Server: 3/11/2016 2:13:10 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -8,29 +8,3 @@ Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
-
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
-
-class SpaceG(App):
-    def __init__(self, width, height):
-        super().__init__(width, height)
-        black=Color(0,1)
-        noline = LineStyle(0,black)
-        bg_asset = RectangleAsset(width, height, noline, black)
-        bg = Sprite(bg_asset, (0,0))
-myapp=SpaceG(SCREEN_WIDTH, SCREEN_HEIGHT)
-
-
-
-class SpaceShip(Sprite):
-    """
-    Animated space ship
-    """
-    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
-        Frame(227,0,292-227,125), 4, 'vertical')
-
-    def __init__(self, position):
-        super().__init__(SpaceShip.asset, position)
-        
-myapp=SpaceShip()
