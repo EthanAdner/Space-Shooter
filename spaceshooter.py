2da5c38cd773aa7304a72914a1e780585dfeaa4b
EthanAdner <ethanda99@gmail.com>
2016-03-29 21:06:45 -0400

    Updated from Brython Server: 3/29/2016 9:06:47 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -128,6 +128,7 @@ class SpaceGame(App):
         SpaceShip((100,100))
         
     def explode(self, xx, yy):
+        return()
         print("abdjhcjhbvehjqbfjgv")
         expl =  ImageAsset("images/explosion.png")
         ex = Sprite(expl, (xx, yy))
